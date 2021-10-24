b0e1230e1793a3637b55161656d7505a0eec9d56
duong thanh khuong <itvtbadboy@gmail.com>
2021-10-24 12:04:03 +0700

    Update map-listing.js

--- js/map-listing.js
+++ js/map-listing.js
@@ -2,7 +2,7 @@
     "use strict";
     function mainMap() {
         function locationData(locationURL, locationImg, locationTitle, locationAddress, locationCategory, locationStarRating, locationRevievsCounter, locationStatus, LocationNhandang) {
-            return ('<div class="map-popup-wrap"><div class="map-popup"><div class="infoBox-close"><i class="fal fa-times"></i></div><!---------- MO HINH ------------><a class="listing-img-content fl-wrap"><div class="infobox-status '+ locationStatus +'">' + locationStatus + '</div><img src="' + locationImg + '" alt="ban do thuong mai"><div class="card-popup-raining map-card-rainting" data-staRrating="' + locationStarRating + '"><span class="map-popup-reviews-count">( ' + locationRevievsCounter + ' đánh giá )</span></div></a> <div class="listing-content"><div class="listing-content-item fl-wrap"><div class="map-popup-location-category ' + locationCategory + '"></div><div class="listing-title fl-wrap"><h4><!---------- TEN DAI DIEN ----------><a href=' + locationURL + ' target="_blank">' + locationTitle + '</a></h4><div class="map-popup-location-info"><i class="fas fa-map-marker-alt"></i>' + locationAddress + '</div></div><div class="map-popup-footer"><!---------- DU AN ----------><a class="main-link"><form action="https://www.google.com/search" method="get" target="_blank"><input style="width: 100%; font-size: 17px; text-align: center;" name="q" type="search" placeholder="tìm theo mô hình"><input name="as_sitesearch" type="hidden" value=' + locationURL + '></form></a><a href=' + LocationNhandang + ' class="infowindow_wishlist-btn"><i class="fal fa-bar-chart"></i></a></div></div></div></div> ')
+            return ('<div class="map-popup-wrap"><div class="map-popup"><div class="infoBox-close"><i class="fal fa-times"></i></div><!---------- MO HINH ------------><a class="listing-img-content fl-wrap"><div class="infobox-status '+ locationStatus +'">' + locationStatus + '</div><img src="' + locationImg + '" alt="ban do thuong mai"><div class="card-popup-raining map-card-rainting" data-staRrating="' + locationStarRating + '"><span class="map-popup-reviews-count" style="font-size: 14px;">( ' + locationRevievsCounter + ' đánh giá )</span></div></a> <div class="listing-content"><div class="listing-content-item fl-wrap"><div class="map-popup-location-category ' + locationCategory + '"></div><div class="listing-title fl-wrap"><h4 style="font-size: 16px;"><!---------- TEN DAI DIEN ----------><a href=' + locationURL + ' target="_blank">' + locationTitle + '</a></h4><div class="map-popup-location-info"><i class="fas fa-map-marker-alt"></i>' + locationAddress + '</div></div><div class="map-popup-footer"><!---------- DU AN ----------><a class="main-link"><form action="https://www.google.com/search" method="get" target="_blank"><input style="width: 100%; font-size: 17px; text-align: center;" name="q" type="search" placeholder="tìm theo mô hình"><input name="as_sitesearch" type="hidden" value=' + locationURL + '></form></a><a href=' + LocationNhandang + ' class="infowindow_wishlist-btn"><i class="fal fa-bar-chart"></i></a></div></div></div></div> ')
         }
 	    //  Map Infoboxes ------------------
         var locations = [
@@ -10,25 +10,25 @@
             [locationData( 'thuongmaitructuyenbrvt.com/', 'https://www.mediafire.com/convkey/489d/1p1fb1flcj5dcgc6g.jpg?size_id=8', 'thương mại brvt', "56 Nguyễn Hữu Huân", 'cafe-cat', "5", "5", "Nhà hàng", "#thuongmai-thuongmaibrvt"), 10.5118216, 107.1994638, 1 , 'https://www.mediafire.com/convkey/489d/1p1fb1flcj5dcgc6g.jpg']
         ];
 	    //   Map Infoboxes end ------------------
-        var map = new google.maps.Map(document.getElementById('bandothuongmai'), {
-            zoom: 14,
-            scrollwheel: false,
+        var map = new google.maps.Map(document.getElementById('map-thuongmaibrvt'), {
+            zoom: 13,
+            scrollwheel: true,
             center: new google.maps.LatLng(10.51, 107.19),
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoomControl: false,
-            mapTypeControl: false,
-            scaleControl: false,
-            panControl: false,
+            zoomControl: true,
+            mapTypeControl: true,
+            scaleControl: true,
+            panControl: true,
             fullscreenControl: true,
-            navigationControl: false,
-            streetViewControl: false,
+            navigationControl: true,
+            streetViewControl: true,
             animation: google.maps.Animation.BOUNCE,
             gestureHandling: 'cooperative',
             styles: [{
                 "featureType": "administrative",
                 "elementType": "labels.text.fill",
                 "stylers": [{
-                    "color": "#444444"
+                    "color": "#444444";
                 }]
             }]
         });
@@ -81,7 +81,7 @@
 	  var markerImg;
 	  var markerCount;
       for (i = 0; i < locations.length; i++) {
-             var labels = '123456789';
+             var labels = 'itvtbadboy';
 		markerImg = locations[i][4];
 		markerCount = locations[i][3] + 1;
         var overlaypositions = new google.maps.LatLng(locations[i][1], locations[i][2]),
@@ -334,7 +334,7 @@ head.insertBefore = function( newElement, referenceElement ) {
     insertBefore.call( head, newElement, referenceElement );
 };	
 	
-	    var map = document.getElementById('bandothuongmai');
+	    var map = document.getElementById('map-thuongmaibrvt');
     if (typeof (map) != 'undefined' && map != null) {
         google.maps.event.addDomListener(window, 'load', mainMap);
     }
