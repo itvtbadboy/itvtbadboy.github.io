06aa38b7a5f205629fc42bb2a1d3b1f254371b83
itvtbadboy <itvtbadboy@gmail.com>
2025-05-18 09:56:50 +0700

    Add files via upload

---
+++ giai-tri/js/thienlongbatbo.js
@@ -0,0 +1,57 @@
+var myPlaylist = [
+	{
+		mp3:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-01.mp3',
+		oga:'#',
+		title:'hội ngộ chung linh',
+		artist:'Đoàn Dự',
+		rating:5,
+		buy:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-01.mp3',
+		price:'0',
+		duration:'52:03',
+		cover:'https://i.pinimg.com/originals/c5/2f/3e/c52f3ee68a4a7cc9c52bbd7cb30fd558.jpg'
+	},
+	{
+		mp3:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-02.mp3',
+		oga:'#',
+		title:'cứu chung linh',
+		artist:'Đoàn Dự',
+		rating:5,
+		buy:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-02.mp3',
+		price:'0',
+		duration:'54:10',
+		cover:'https://i.pinimg.com/originals/c5/2f/3e/c52f3ee68a4a7cc9c52bbd7cb30fd558.jpg'
+	},
+	{
+		mp3:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-03.mp3',
+		oga:'#',
+		title:'say đắm tượng mỹ nhân',
+		artist:'Đoàn Dự',
+		rating:5,
+		buy:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-03.mp3',
+		price:'0',
+		duration:'52:24',
+		cover:'https://i.pinimg.com/originals/c5/2f/3e/c52f3ee68a4a7cc9c52bbd7cb30fd558.jpg'
+	},
+	{
+		mp3:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-04.mp3',
+		oga:'#',
+		title:'bí kíp võ học tiêu dao',
+		artist:'Đoàn Dự',
+		rating:5,
+		buy:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-04.mp3',
+		price:'0',
+		duration:'53:46',
+		cover:'https://i.pinimg.com/originals/c5/2f/3e/c52f3ee68a4a7cc9c52bbd7cb30fd558.jpg'
+	},
+	{
+		mp3:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-05.mp3',
+		oga:'#',
+		title:'hội ngộ mộc uyển thanh',
+		artist:'Đoàn Dự',
+		rating:5,
+		buy:'https://storage.googleapis.com/khuong-itvtbadboy-8383.appspot.com/podcast/tlbb/thien-long-bat-bo-05.mp3',
+		price:'0',
+		duration:'54:43',
+		cover:'https://i.pinimg.com/originals/c5/2f/3e/c52f3ee68a4a7cc9c52bbd7cb30fd558.jpg'
+	}
+];
