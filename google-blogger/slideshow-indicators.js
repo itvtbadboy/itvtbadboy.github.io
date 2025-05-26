5fcd5c1e86de31f3a450a6b6c45219789538ba07
itvtbadboy <itvtbadboy@gmail.com>
2025-05-26 09:54:04 +0800

    Update slideshow-indicators.js

--- google-blogger/slideshow-indicators.js
+++ google-blogger/slideshow-indicators.js
@@ -4,8 +4,8 @@ function currentDiv(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides", "mySlides2");
-  var dots = document.getElementsByClassName("demo", "ao_dai_do");
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
